ca05cee989b4c3f224a1ed7b89a49f76a56a0548
Alisson Carvalho <webalissoncs@gmail.com>
2017-06-28 10:28:18 -0300

    unnecessary code at app.js

--- app/application.js
+++ app/application.js
@@ -10,8 +10,6 @@ const bodyParser = require('body-parser')
 // application starup
 const app = express()
 
-import Movie from './entity/Movie'
-
 // assign the bodyParser module
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,10 +20,14 @@ app.use( '*', (req,res) => {
   res.status(404).json({
     message: 'Non existent route'
   })
-} )
+})
+
+// error handler
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).json({message: 'Something broke!'});
+  res.status(500).json({
+    message: 'Something broke!'
+  });
 })
 
 module.exports = app
